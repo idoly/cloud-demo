84cb5fac1b7932e843ba2d82a72c99eccceec6f3
unknown <yanxin>
2025-08-07 09:19:46 +0800

    first commit

--- service-c/src/main/java/xyz/idoly/demo/C.java
+++ service-c/src/main/java/xyz/idoly/demo/C.java
@@ -1,7 +1,5 @@
 package xyz.idoly.demo;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
